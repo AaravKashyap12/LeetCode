d04a1d5e19a09bda8866f0cb5195a0ef15b1da6d
AaravKashyap12 <142650856+AaravKashyap12@users.noreply.github.com>
2025-12-08 23:51:38 +0530

    Time: 0 ms (100%), Space: 48.9 MB (12.31%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+    int i = 2;
+    int j = 2;
+    
+    for(;j<nums.length;j++){
+        if(nums[j]!=nums[i-2]){
+            nums[i]=nums[j];
+            i++;
+        }
+    }
+    return i;
+    }
+}
