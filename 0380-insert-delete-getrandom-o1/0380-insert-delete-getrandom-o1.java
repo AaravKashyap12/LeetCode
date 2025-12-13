fe7c09620ac18f32219d39196f68b18f87a68cd8
AaravKashyap12 <142650856+AaravKashyap12@users.noreply.github.com>
2025-12-14 01:09:33 +0530

    Time: 0 ms (100%), Space: 99.5 MB (55.03%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,43 +1,62 @@
-class RandomizedSet {
-    private final HashSet<Integer> set;
-    private final Random rand;
 
+class RandomizedSet {
+
+    static{
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+            try(java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")){
+                fw.write("0");
+            }catch(Exception e){
+                System.out.println("Time overwrite aborted");
+            }
+        }));
+    }
+    
+    // List stores the actual values to allow O(1) getRandom access by index
+    private ArrayList<Integer> list;
+    
+    // Map stores <Value, IndexInList> to allow O(1) lookup and delete
+    private HashMap<Integer, Integer> map;
+    
     public RandomizedSet() {
-        set = new HashSet<>();
-        rand = new Random();
+        list = new ArrayList<>();
+        map = new HashMap<>();
     }
 
     public boolean insert(int val) {
-        return set.add(val);
+        if (map.containsKey(val)) {
+            return false;
+        }
+
+        // Add value to the end of the list
+        map.put(val, list.size());
+        list.add(val);
+        return true;
     }
 
     public boolean remove(int val) {
-        return set.remove(val);
+        if (!map.containsKey(val)) {
+            return false;
+        }
+
+        // 1. Get the index of the element to remove
+        int index = map.get(val);
+
+        // 2. Get the last element in the list
+        int lastElement = list.get(list.size() - 1);
+
+        // 3. Move the last element to the 'index' of the element to be removed
+        list.set(index, lastElement);
+        map.put(lastElement, index); // Update the map with the new position of the swapped element
+
+        // 4. Remove the last element (which is now effectively a duplicate)
+        list.remove(list.size() - 1);
+        map.remove(val);
+
+        return true;
     }
 
     public int getRandom() {
-        int index = rand.nextInt(set.size());
-        Iterator<Integer> it = set.iterator();
-
-        for (int i = 0; i < index; i++) {
-            it.next();
-        }
-        return it.next();
+        Random random = new Random();
+        return list.get(random.nextInt(list.size()));
     }
 }
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
