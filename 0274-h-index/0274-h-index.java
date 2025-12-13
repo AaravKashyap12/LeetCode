038448cbfa44f7133073a71b93d9d3449a5ed028
AaravKashyap12 <142650856+AaravKashyap12@users.noreply.github.com>
2025-12-14 00:59:40 +0530

    Time: 6 ms (17.92%), Space: 43.6 MB (14%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void sort(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        for (int i = 0; i < n / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - 1 - i];
+            arr[n - 1 - i] = temp;
+        }
+    }
+
+    public int hIndex(int[] citations) {
+        sort(citations);
+        int count = 0;
+        for(int i=0;i<citations.length;i++){
+            if(citations[i]>=i+1){
+                count++;
+            }
+        }
+        return count;
+    }
+}
