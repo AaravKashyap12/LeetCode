ac496cf883cba34875830c21a8016b8e6eecb9f8
AaravKashyap12 <142650856+AaravKashyap12@users.noreply.github.com>
2025-12-16 00:59:35 +0530

    Time: 4 ms (24.98%), Space: 47.1 MB (67.25%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int count = 0;
+        int[] candies = new int[ratings.length];
+
+        for (int i = 0; i < candies.length; i++) {
+            candies[i] = 1;
+        }
+
+        for (int i = 1; i < candies.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = candies.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i + 1] + 1, candies[i]);
+            }
+        }
+
+        for (int i=0;i<candies.length;i++) {
+            count += candies[i];
+        }
+        return count;
+    }
+}
