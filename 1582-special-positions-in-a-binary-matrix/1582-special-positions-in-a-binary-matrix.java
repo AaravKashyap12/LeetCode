a68b77966b00affe7d95b167a268848b1111f7a0
Aarav Kashyap Singh <142650856+AaravKashyap12@users.noreply.github.com>
2026-03-04 12:40:23 +0530

    Time: 2 ms (94.84%), Space: 47 MB (91.99%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -15,7 +15,7 @@ class Solution {
     for(int i=0;i<mat.length;i++){
         for(int j=0;j<mat[0].length;j++){
             if(mat[i][j]==1 && rowcnt[i]==1 && colcnt[j]==1){
-                ans++;
+                ans++; 
             }
         }
     } 
