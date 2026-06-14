dcfa20dd6938968fee2e572c9b92456019499186
Aarav Kashyap Singh <142650856+AaravKashyap12@users.noreply.github.com>
2026-06-14 22:08:06 +0530

    Time: 13 ms (9.55%), Space: 118.3 MB (5.49%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-    Stack<Integer> stack = new Stack<>();
+    Deque<Integer> stack = new ArrayDeque<>();
     int max = Integer.MIN_VALUE;
     ListNode curr = head;
 
