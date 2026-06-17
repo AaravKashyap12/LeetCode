2409514c97958f568fe0dc9ff967f7ee0e2f3cd1
Aarav Kashyap Singh <142650856+AaravKashyap12@users.noreply.github.com>
2026-06-18 00:05:21 +0530

    Time: 54 ms (11.54%), Space: 48.2 MB (15.38%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
@@ -1,28 +1,27 @@
-/*
- * @lc app=leetcode id=3614 lang=java
- *
- * [3614] Process String with Special Operations II
- */
-
 class Solution {
     public char processStr(String s, long k) {
-        return CompletableFuture.supplyAsync(() -> Optional.of(new long[] { 0, k, '.', 0 }).map(st -> (char) (s.chars()
-                .mapToLong(c -> (st[0] = Character.isLowerCase(c) ? st[0] + 1
-                        : (c == '*' && st[0] > 0 ? st[0] - 1 : (c == '#' ? st[0] * 2 : st[0]))) * 0L)
-                .sum() * 0L
-                + (st[1] >= st[0] ? 0L
-                        : IntStream.range(0, s.length()).map(inv -> s.length() - 1 - inv)
-                                .mapToLong(i -> st[3] == 1 ? 0L
-                                        : (Character.isLowerCase(s.charAt(i))
-                                                ? (st[1] == st[0] - 1 ? (st[2] = s.charAt(i)) * 0L + (st[3] = 1) * 0L
-                                                        : (st[0]--) * 0L)
-                                                : (s.charAt(i) == '*' ? (st[0]++) * 0L
-                                                        : (s.charAt(i) == '#'
-                                                                ? (st[0] /= 2) * 0L
-                                                                        + (st[1] >= st[0] ? (st[1] -= st[0]) : 0L) * 0L
-                                                                : (s.charAt(i) == '%' ? (st[1] = st[0] - 1 - st[1]) * 0L
-                                                                        : 0L)))))
-                                .sum() * 0L)
-                + st[2])).get()).join();
+        long len = 0;
+        for (char c : s.toCharArray()) {
+            if (Character.isLowerCase(c)) len++;
+            else if (c == '*' && len > 0) len--;
+            else if (c == '#') len *= 2;
+        }
+        if (k >= len) return '.';
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (Character.isLowerCase(c)) {
+                if (k == len - 1) return c;
+                len--;
+            } else if (c == '*') {
+                len++;
+            } else if (c == '#') {
+                len /= 2;
+                if (k >= len) k -= len;
+            } else if (c == '%') {
+                k = len - 1 - k;
+            }
+        }
+        return '.';
     }
 }
